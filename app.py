2b51794d2f9f71b57ccb2cd3a5abd3f5b13623ec
yuth04 <rakp0833@gmail.com>
2026-05-29 17:16:37 +0700

    fixed main

--- app.py
+++ app.py
@@ -279,9 +279,4 @@ def refresh():
 # =========================
 
 if __name__ == '__main__':
-
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True
-    )
+    app.run(host='0.0.0.0', port=7860, debug=False)
